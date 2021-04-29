e52ca1f349f75e20cc31f12685db666631e9a00f
Jia-Jingnan <875480307@qq.com>
2021-04-29 19:20:27 +0800

    0429-初始化YouTest项目

---
+++ src/main/java/com/lilith/youtest/YoutestApplication.java
@@ -0,0 +1,13 @@
+package com.lilith.youtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lilith/youtest/YoutestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilith.youtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
