1f3453a823adc19951ac89677f532d2da52cd104
Jia-Jingnan <875480307@qq.com>
2021-04-29 21:03:50 +0800

    0429-在启动类上添加MapperScan扫描Dao层接口，添加@EnableSwagger2，启用Swagger2接口文档

--- src/main/java/com/lilith/youtest/YoutestApplication.java
+++ src/main/java/com/lilith/youtest/YoutestApplication.java
@@ -1,9 +1,13 @@
 package com.lilith.youtest;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@MapperScan(basePackages = "com.lilith.youtest")
 public class YoutestApplication {
 
     public static void main(String[] args) {
