12f58619c3e3206c7eaeca3a8fca28d0e6afb316
Jia-Jingnan <875480307@qq.com>
2021-05-01 18:23:55 +0800

    0501-增加单元测试的基础类，后续所有的单元测试都继承BaseTest类

---
+++ src/test/java/com/lilith/youtest/BaseTest.java
@@ -0,0 +1,14 @@
+package com.lilith.youtest;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 下午6:19 2021/5/1
+ */
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BaseTest {
+}
