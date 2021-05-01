bfeb12bc894aaf2380ed14c57ca80dbd77cfb2bb
Jia-Jingnan <875480307@qq.com>
2021-05-02 01:00:19 +0800

    0502-修改一个Bug

--- src/main/java/com/lilith/youtest/handler/YoutestMetaObjectHandler.java
+++ src/main/java/com/lilith/youtest/handler/YoutestMetaObjectHandler.java
@@ -2,6 +2,7 @@ package com.lilith.youtest.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
@@ -9,6 +10,7 @@ import java.util.Date;
  * @Author:JiaJingnan
  * @Date: 下午8:44 2021/5/1
  */
+@Component
 public class YoutestMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
