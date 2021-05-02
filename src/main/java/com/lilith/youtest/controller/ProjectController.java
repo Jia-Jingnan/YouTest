b9c23a8e273bc3b06944c3307c370e9d90ead2a1
Jia-Jingnan <875480307@qq.com>
2021-05-02 21:42:12 +0800

    0502-增加add方法和查询方法

--- src/main/java/com/lilith/youtest/controller/ProjectController.java
+++ src/main/java/com/lilith/youtest/controller/ProjectController.java
@@ -4,14 +4,13 @@ package com.lilith.youtest.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.lilith.youtest.common.CommonResult;
 import com.lilith.youtest.entity.Project;
+import com.lilith.youtest.entity.User;
 import com.lilith.youtest.service.ProjectService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +29,25 @@ public class ProjectController {
 
     @Autowired
     private ProjectService projectService;
+
+    @ApiOperation(value = "根据ID查询项目", httpMethod = "GET")
+    @GetMapping("/{projectId}")
+    public CommonResult getById(@PathVariable("projectId") Integer projectId){
+        CommonResult commonResult = null;
+        Project project = projectService.getById(projectId);
+        commonResult = new CommonResult("1",project,"成功");
+        return commonResult;
+    }
+
+    @ApiOperation(value = "项目新增方法",httpMethod = "POST")
+    @PostMapping("add")
+    public CommonResult add(Project project){
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        project.setCreateUser(user.getId());
+        projectService.save(project);
+
+        return new CommonResult("1","项目添加成功");
+    }
 
     @ApiOperation(value = "项目列表", httpMethod = "GET")
     @GetMapping("list")
