1e84b2a019e3227a205c08f659f9a22ce408c5b7
Jia-Jingnan <875480307@qq.com>
2021-05-03 18:11:41 +0800

    0503-先查询api_classification分类信息，再根据分类ID查询该分类下的接口

--- src/main/java/com/lilith/youtest/mapper/ApiClassificationMapper.java
+++ src/main/java/com/lilith/youtest/mapper/ApiClassificationMapper.java
@@ -2,6 +2,14 @@ package com.lilith.youtest.mapper;
 
 import com.lilith.youtest.entity.ApiClassification;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lilith.youtest.vo.ApiClassificationVO;
+import org.apache.ibatis.annotations.Many;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.type.JdbcType;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,5 +21,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface ApiClassificationMapper extends BaseMapper<ApiClassification> {
 
-    // 两表 延迟加载 先查询分类信息，
+    // 两表 延迟加载 先查询分类信息List<Api> 按需加载（即此时再查询另外一张表）
+    @Select("SELECT * FROM api_classification WHERE project_id = #{projectId}")
+    @Results({
+            @Result(column = "id", property = "id"),
+            @Result(column = "project_id", property = "projectId"),
+            @Result(column = "name", property = "name"),
+            @Result(column = "id", property = "apis",many = @Many(select = "com.lilith.youtest.ApiMapper.findApi")),
+    })
+    List<ApiClassificationVO> getWithApi(Integer projectId);
 }
--- src/main/java/com/lilith/youtest/mapper/ApiMapper.java
+++ src/main/java/com/lilith/youtest/mapper/ApiMapper.java
@@ -2,6 +2,9 @@ package com.lilith.youtest.mapper;
 
 import com.lilith.youtest.entity.Api;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2021-05-03
  */
 public interface ApiMapper extends BaseMapper<Api> {
+
+    @Select("SELECT * FROM api WHERE api_classification_id = #{apiClassificationId}")
+    List<Api> findApi(Integer apiClassificationId);
 
 }
