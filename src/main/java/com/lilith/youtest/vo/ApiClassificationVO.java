19f0d1b6f190e4aab7c7085115fdab643d3316cf
Jia-Jingnan <875480307@qq.com>
2021-05-05 00:32:46 +0800

    0505-修改分类下接口不展示的Bug，原因是缺少Getter/Setter添加@Data注解，可以解决

--- src/main/java/com/lilith/youtest/vo/ApiClassificationVO.java
+++ src/main/java/com/lilith/youtest/vo/ApiClassificationVO.java
@@ -2,6 +2,7 @@ package com.lilith.youtest.vo;
 
 import com.lilith.youtest.entity.Api;
 import com.lilith.youtest.entity.ApiClassification;
+import lombok.Data;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import java.util.List;
  * @Author:JiaJingnan
  * @Date: 下午5:53 2021/5/3
  */
+@Data
 public class ApiClassificationVO extends ApiClassification {
 
     // 关联对象
