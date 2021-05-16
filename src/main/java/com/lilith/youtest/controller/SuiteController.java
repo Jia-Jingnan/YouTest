2135bd37017224709560cfc984f0eea4882bf335
Jia-Jingnan <875480307@qq.com>
2021-05-16 15:38:21 +0800

    0516-增加list方法，查询出该项目下所有的测试集

--- src/main/java/com/lilith/youtest/controller/SuiteController.java
+++ src/main/java/com/lilith/youtest/controller/SuiteController.java
@@ -1,9 +1,18 @@
 package com.lilith.youtest.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.lilith.youtest.common.CommonResult;
+import com.lilith.youtest.entity.Suite;
+import com.lilith.youtest.service.SuiteService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +25,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/suite")
 public class SuiteController {
+
+    @Autowired
+    private SuiteService suiteService;
+
+
+    @ApiOperation(value = "根据projectId获取测试集", httpMethod = "GET")
+    @GetMapping("/list")
+    public CommonResult list(String projectId){
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("project_id",projectId);
+        List<Suite> list = suiteService.list(queryWrapper);
+        return new CommonResult("1",list);
+    }
+
+
 
 }
