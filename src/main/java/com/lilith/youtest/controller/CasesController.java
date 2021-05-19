abca56544dda3f4b0f7ad3d066f1fdbfb5cd994d
Jia-Jingnan <875480307@qq.com>
2021-05-20 01:00:11 +0800

    0520-新增测试集add方法

--- src/main/java/com/lilith/youtest/controller/CasesController.java
+++ src/main/java/com/lilith/youtest/controller/CasesController.java
@@ -1,9 +1,23 @@
 package com.lilith.youtest.controller;
 
 
+import com.lilith.youtest.common.CommonResult;
+import com.lilith.youtest.entity.ApiRequestParam;
+import com.lilith.youtest.entity.CaseParamValue;
+import com.lilith.youtest.entity.Cases;
+import com.lilith.youtest.service.CaseParamValueService;
+import com.lilith.youtest.service.CasesService;
+import com.lilith.youtest.vo.ApiVO;
+import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Case;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +30,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cases")
 public class CasesController {
+
+    @Autowired
+    private CasesService casesService;
+
+    @Autowired
+    CaseParamValueService caseParamValueService;
+
+    @ApiOperation(value = "添加测试集", httpMethod = "POST")
+    @PostMapping("/add")
+    public CommonResult add(Cases caseVo, ApiVO apiVO){
+
+        // 添加到cases
+        casesService.save(caseVo);
+        //2.批量添加到case_param_value
+        List<ApiRequestParam> requestParamList = apiVO.getRequestParams();
+        List<CaseParamValue> caseParamValueList = new ArrayList<>();
+        for (ApiRequestParam requestParam : requestParamList) {
+            CaseParamValue caseParamValue = new CaseParamValue();
+            caseParamValue.setCaseId(apiVO.getId());
+            caseParamValue.setApiRequestParamId(requestParam.getId());
+            caseParamValue.setApiRequestParamValue(requestParam.getValue());
+            caseParamValueList.add(caseParamValue);
+        }
+
+        caseParamValueService.saveBatch(caseParamValueList);
+        return new CommonResult("1","添加测试集成功");
+
+    }
 
 }
